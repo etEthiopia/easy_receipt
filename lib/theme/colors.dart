7b65afcb7d098f988fe8a9c55f87a787ebed601b
Dagmawi Negussu <5902034-daginegussu@users.noreply.gitlab.com>
2020-12-17 15:10:09 +0300

    color and firebase json

---
+++ lib/theme/colors.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+Color dbackground = Colors.black;
+
+Color dbackground1 = Colors.grey[700];
+
+Color dbackground2 = Colors.grey[800];
+
+Color dbackground3 = Colors.grey[900];
+
+Color dark = Color(0Xff9d8189);
+
+Color primary = Color(0Xfff4acb7);
+
+Color light = Color(0Xffffcad4);
+
+Color accent = Color(0Xffffe5d9);
+
+
