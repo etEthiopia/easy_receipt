4f4aad1d835630da50a695b2f8373b629b491ccf
Dagmawi Negussu <5902034-daginegussu@users.noreply.gitlab.com>
2020-12-18 20:49:17 +0300

    sign in worked

--- lib/receipt_review.dart
+++ lib/receipt_review.dart
@@ -2,6 +2,7 @@ import 'dart:io';
 import 'package:firebase_auth/firebase_auth.dart';
 
 import 'package:flutter/material.dart';
+import 'package:google_sign_in/google_sign_in.dart';
 import 'package:image_picker/image_picker.dart';
 
 class ReceiptPreviewScreen extends StatefulWidget {
@@ -14,6 +15,8 @@ class _ReceiptPreviewScreen extends State<ReceiptPreviewScreen> {
   File _imageFile;
   bool showImageOptions = false;
   FirebaseAuth auth = FirebaseAuth.instance;
+  final FirebaseAuth firebaseAuth = FirebaseAuth.instance;
+  AuthResult user;
 
   @override
   void initState() {
@@ -23,12 +26,15 @@ class _ReceiptPreviewScreen extends State<ReceiptPreviewScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: SingleChildScrollView(
-          child: Container(
-            alignment: Alignment.topCenter,
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: <Widget>[_setImageView()],
+        body: SafeArea(
+          child: SingleChildScrollView(
+            child: Container(
+              alignment: Alignment.topCenter,
+              padding: EdgeInsets.only(top: 10.0, left: 5.0, right: 5.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: <Widget>[_setImageView()],
+              ),
             ),
           ),
         ),
@@ -107,7 +113,50 @@ class _ReceiptPreviewScreen extends State<ReceiptPreviewScreen> {
     if (_imageFile != null) {
       return Image.file(_imageFile, width: 500, height: 500);
     } else {
-      return Text("Please select an image");
+      return Column(
+        children: [
+          Text("Please select an image"),
+          user == null
+              ? FlatButton(
+                  onPressed: () async {
+                    signInWithGoogle();
+                  },
+                  child: Text("Sign In With Google"))
+              : FlatButton(
+                  onPressed: () async {
+                    signOut();
+                  },
+                  child: Text("Sign Out")),
+          Text((user != null) ? user.user.displayName : "Not logged in"),
+        ],
+      );
     }
+  }
+
+  void signInWithGoogle() async {
+    final GoogleSignInAccount googleUser = await GoogleSignIn().signIn();
+    final GoogleSignInAuthentication googleAuth =
+        await googleUser.authentication;
+    //Create Google Auth credentials to pass to Firebase
+    final AuthCredential googleAuthCredential =
+        GoogleAuthProvider.getCredential(
+      accessToken: googleAuth.accessToken,
+      idToken: googleAuth.idToken,
+    );
+
+    //Authenticate against Firebase with Google credentials
+    await firebaseAuth
+        .signInWithCredential(googleAuthCredential)
+        .then((userCredential) => {
+              setState(() => {user = userCredential})
+            });
+  }
+
+  void signOut() async {
+    await firebaseAuth.signOut().then((value) => {
+          setState(() {
+            user = null;
+          })
+        });
   }
 }
